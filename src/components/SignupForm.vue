20a2225e20979bd0f47fb2962db93ac9ee29a359
arielglvz <arielglvz@gmail.com>
2024-11-19 01:51:36 +0800

    Setting up and added modules from 4 lessons

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,82 @@
+<template>
+  <form>
+    <!-- v-model can be used on a component to implement a two-way binding. -->
+    <!-- Two-way data binding means values are bound 2 ways between the template inputs and the component data itself and vise-versa-->
+    <!-- Example: if you any data in an input it will automatically be the value in the component data 
+         and if you put any value on the component data it will be the value in the v-model -->
+    <label>Email:</label>
+    <input type="email" required v-model="email"/>
+    
+    <label>Password:</label> 
+    <input type="password" required v-model="password"/>
+    
+    <!-- Select Boxes -->
+    <label>Role:</label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
+
+    <!-- Checkbox -->
+    <div class="terms">
+      <input type="checkbox" v-model="terms" required>
+      <label>Accept terms and conditions</label>
+    </div>
+
+  </form>
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
+  <p>Terms accepted: {{ terms }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      role: 'designer',
+      terms: false,
+      // names: []
+    }
+  }
+}
+</script>
+
+<style>
+  form {
+    max-width: 420px;
+    margin: 30px auto;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+  }
+  label {
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+  }
+  input,
+  select {
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+  }
+  input[type="checkbox"] {
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
+  }
+</style>
